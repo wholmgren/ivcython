0d391a6b85357642ee1364a1aa80b2aa9ab4dcff
Will Holmgren <william.holmgren@gmail.com>
2018-02-05 18:41:00 -0700

    cdefs, redo looping

--- newton_example.pyx
+++ newton_example.pyx
@@ -1,23 +1,33 @@
 from __future__ import division, print_function, absolute_import
-from math import exp, sin
+
+import numpy as np
+cimport numpy as np
+
+from libc.math cimport exp
 
 #from scipy.optimize.cython_optimize cimport zeros
 
 # searches for zeros.pxd
 cimport zeros
 
-NUM_OF_IRRAD = 10
-IL = [sin(il) + 6.0 for il in range(NUM_OF_IRRAD)]
+NUM_OF_IRRAD = 100000
+IL = np.sin(np.linspace(0, NUM_OF_IRRAD, NUM_OF_IRRAD)) + 6
 
 
 # governing equations
-cdef double f_solarcell(double i, tuple args):
+cdef double f_solarcell(double i, tuple args) :
+    cdef double v, il, io, rs, rsh, vt
     v, il, io, rs, rsh, vt = args
     vd = v + i * rs
     return il - io * (exp(vd / vt) - 1.0) - vd / rsh - i
 
 
+cdef double fprime_no_tuple(double i, double v, double il, double io, double rs, double rsh, double vt):
+    return -io * exp((v + i * rs) / vt) * rs / vt - rs / rsh - 1
+
+
 cdef double fprime(double i, tuple args):
+    cdef double v, il, io, rs, rsh, vt
     v, il, io, rs, rsh, vt = args
     return -io * exp((v + i * rs) / vt) * rs / vt - rs / rsh - 1
 
@@ -30,5 +40,10 @@ cdef double solarcell_newton(tuple args):
 
 # cython
 def bench_cython_newton(v=5.25, il=IL, args=(1e-09, 0.004, 10, 0.27456)):
-    return map(solarcell_newton,
-               ((v, il_,) + args for il_ in il))
+    cdef int N = IL.shape[0]
+    cdef double[:] out = np.zeros(N)
+
+    for i in range(N):
+        out[i] = solarcell_newton((v, il[i],) + args)
+
+    return np.asarray(out)
