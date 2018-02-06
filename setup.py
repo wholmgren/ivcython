08e3b4f56331dd5ada541c0ea0852038c391bfd6
Will Holmgren <william.holmgren@gmail.com>
2018-02-05 18:41:25 -0700

    numpy

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
+
+import numpy as np
 
 setup(
     name='ivcython',
     ext_modules=cythonize("*.pyx"),
+    include_dirs=[np.get_include()]
 )
